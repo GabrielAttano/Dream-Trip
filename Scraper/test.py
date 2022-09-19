f30b3f3c07301c89e538fe7b3f915a2e69fc4344
Gabriel Sá <t6w311@gmail.com>
2022-09-18 23:27:21 -0300

    New test case

--- Scraper/test.py
+++ Scraper/test.py
@@ -1,4 +1,15 @@
-from scrap import DecolarDestinations, DecolarScraper
+from scraper import DecolarDestinations, DecolarScraper
+from selenium import webdriver
+import time
 
-scraper = DecolarScraper()
-scraper.get_flight_prices(DecolarDestinations.BRASILIA,DecolarDestinations.PORTO_ALEGRE, "2022-09-30")
+def test_scraping_time(total_destinations, end_destination: DecolarDestinations, departure_date, driver: webdriver.Chrome):
+    scraper = DecolarScraper()
+    destinations = [x for x in DecolarDestinations]
+    start = time.time()
+    dict_result = scraper.get_multiple_flight_prices(destinations[:total_destinations], end_destination, departure_date, driver)
+    end = time.time()
+    print(dict_result)
+    print(f"total time = {end - start}")
+
+driver = webdriver.Chrome()
+test_scraping_time(10, DecolarDestinations.PALMAS, "2022-09-30", driver)
